4a55392f26a3f6e5f21771b8913b7f17e12318ce
dishap-dk <dishapushpad4@gmail.com>
2022-08-08 23:50:51 +0530

    my second assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash=require('lodash')
 const abc = require('../introduction/intro')
 const logg=require('../logger/logger')
 const show=require('../validator/formatter')
@@ -6,17 +7,20 @@ const dates=require('../util/helper')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logg.name1
-    logg.func1
-    show.trim1
-    show.lower1
-    show.upcase1
-    dates.print1
-    dates.month
-    dates.BatchInfo1
     
+    let month=["jan","feb","mar","apr","may","jun","jul","aug","sept","oct","nov","dec"]
+    console.log(lodash.chunk(month,3))
+     let arr=[1,3,5,7,9,11,13,15,17,19]
+     console.log(lodash.tail(arr))
+     let m=[1,2,3]
+     let n=[2,3,4]
+     let o=[3,4,5]
+     let p=[4,5,6]
+     let q=[5,6,7]
+     console.log(lodash.union(m,n,o,p,q))
+     let movies = [["horror","The Shining"],["drama","Titanic"], ["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+     console.log(lodash.fromPairs(movies))
+
     res.send('My second ever api!')
 });
 
@@ -29,4 +33,5 @@ router.get('/give-me-students-data',function(req, res){
 
 })
 module.exports = router;
+
 // adding this comment for no reason
