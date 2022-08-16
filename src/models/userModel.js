d13d4106af3d6f4e39df4026c8e2d1edda66b12a
dishap-dk <dishapushpad4@gmail.com>
2022-08-16 20:20:09 +0530

    mongodb_bookapi

---
+++ src/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+const userSchema = new mongoose.Schema({
+bookName:String,
+authorName:String,
+category:String,
+year:String
+
+},{timestamps:true});
+ 
+module.exports=mongoose.model('user',userSchema);//users
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
