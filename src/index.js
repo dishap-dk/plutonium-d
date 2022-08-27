9931301ed7ed39ae2b4ba0276cb33f666ab83bf9
dishap-dk <dishapushpad4@gmail.com>
2022-08-27 15:14:21 +0530

    iddleware/Assignment1

--- src/index.js
+++ src/index.js
@@ -13,8 +13,17 @@ mongoose.connect("mongodb+srv://Dishap:gn6kyXLuhnBE1EJK@cluster0.bwp65jf.mongodb
 })
 .then(() => console.log("MongoDB is connected"))
 .catch( err => console.log(err))
-
+app.use(
+    function(req,res,next){
+       let date=new Date()
+       let address=req.ip
+       let uadd=req.url
+console.log(date,address,uadd)
+        next()
+    }
+)
 app.use('/', route);
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,12 +7,35 @@ const bookcontroller3=require('../controller/bookcontroller3')
 const authorcontroll=require('../controller/authorcontroll')
 const publishercontroll=require('../controller/publishercontroll')
 
+// const mid1=function(req,res,next){
+//     let loggedIn=false
+// if(loggedIn=true){
+//     next()
+// }else{
+//     res.send("please login")
+// }
+// }
+
+// const mid2=function(req,res,next){
+//     console.log("hii, this mi2")
+//     next()
+// }
+
+// const mid3=function(req,res,next){
+//     let time=new Date()
+  
+//     console.log("hii, this mi2",time)
+//     next()
+// }
+
+
+
 
 router.post('/createAuthor',authorcontroll.createAuthor)
 router.post('/createPublisher',publishercontroll.createPublisher)
 router.post('/createBook',bookcontroller3.createBook)
 router.get('/getBook',bookcontroller3.getBook)
-router.get('/getLinkedData',bookcontroller3.getLinkedData)
+router.get('/getLinkedData', bookcontroller3.getLinkedData)
 router.put('/apiBooks',bookcontroller3.apiBooks)
 
 
