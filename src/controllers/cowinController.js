1cf7aae3d63b7bfe0e374744048a990c4c7b081c
dishap-dk <dishapushpad4@gmail.com>
2022-09-02 19:35:05 +0530

    pending-axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,20 +1,66 @@
 const axios=require('axios')
 
 
-const districtId=async function(req,res){
+const states=async function(req,res){
     try{
         let result=await axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
         console.log(result)
         let data=result.data
         res.status(200).send({msg:data})
 
-
-
-
-
 }catch(err){
         res.status(500).send({error:err.message})
 }
 }
+//------------------------------------findby district-------------------------------------
+const getDistrictId= async function(req,res){
+        try{
+let id=req.params.stateId
+let options={
+        method:"get",
+        url:`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`,
+}
+let result=await axios(options);
+console.log(result);
+let data=result.data;
+res.status(200).send({status:true,msg:data})
+        }
+        catch(err){
+                res.status(500).send({status:false,error:err.message})        
+        }
+}
 
-module.exports.districtId=districtId
+
+//----------------------------------------------- 3rd find by pin------------------------------------------------
+
+const getByPin= async function(req,res){
+        try{
+let pin=req.query.pincode
+let date=req.query.date
+
+let options={
+        method:"get",
+         url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+}
+let result=await axios(options);
+console.log(result);
+let data=result.data;
+res.status(200).send({status:true,msg:data})
+        }
+        catch(err){
+                res.status(500).send({status:false,error:err.message})        
+        }
+}
+
+
+
+
+
+
+
+
+
+
+module.exports.states=states
+module.exports.getDistrictId=getDistrictId
+module.exports. getByPin=getByPin
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,33 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController')
-const userController = require('../controllers/userController')
-const orderController = require('../controllers/orderController')
-const middleware = require('../middlewares/validator')
-//============================= TEST API ================================
-
-router.get("/test-me", function (req, res) { res.send("My first ever api!")})
-
-///============================= USER API =================================
-
-router.post('/createUser',middleware.validateHeader , userController.createUser)
-
-
-//=============================== PRODUCT API ===================================================
-
-router.post('/createProduct',productController.createProduct)
+const cowinController=require('../controllers/cowinController')
 
 
 
-//================================= ORDER API ====================================
 
-router.post('/createOrder',middleware.validateHeader,orderController.createOrder)
+router.get('/states',cowinController.states)
+router.get('/getDistrictId/:stateId',cowinController.getDistrictId)
+router.get('/getByPin',cowinController.getByPin)
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
-
-
-
-
-
-
-// adding this comment for no reason
